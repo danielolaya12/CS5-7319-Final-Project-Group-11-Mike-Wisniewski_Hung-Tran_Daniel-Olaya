1ab2eee8c7c111e0f28e6eb00cfd4b41fad4db21
Mike Wisniewski <wisniemj@gmail.com>
2024-04-06 20:01:54 -0400

    Ran 3 times

--- src/main/java/org/healthetl/filters/S3Writer.java
+++ src/main/java/org/healthetl/filters/S3Writer.java
@@ -76,7 +76,6 @@ public class S3Writer extends Filter {
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(csvData.getBytes(StandardCharsets.UTF_8).length);
         String destinationLocation = bucketName + pathName;
-        log.info(destinationLocation);
         s3Client.putObject(new PutObjectRequest(destinationLocation, "output.csv", inputStream, metadata));
         System.out.println("Successfully wrote data to S3.");
     }
